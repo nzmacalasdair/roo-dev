b161c98a394a3e48b052ef1bfe370b471bbd0a5c
Neil MacAlasdair <macalasdair@ubu-n-50797e58862a584d.ebi.ac.uk>
2025-10-24 17:16:40 +0100

    GPU bugfix

--- remove_recombination/pairwise_comparisons.py
+++ remove_recombination/pairwise_comparisons.py
@@ -34,7 +34,9 @@ def get_pairwise_differences(seq1, seq2, gpu):
             return result
         else:
             mask = seq1 != 45
-            consecutive_bases = cp.diff(cp.concatenate(([0], mask.astype(int), [0])))
+            consecutive_bases = cp.diff(cp.concatenate((cp.array([0]), 
+                                                        mask.astype(int), 
+                                                        cp.array([0]))))
             start_positions = cp.where(consecutive_bases == 1)[0]
             end_positions = cp.where(consecutive_bases == -1)[0]
             lengths = end_positions - start_positions
