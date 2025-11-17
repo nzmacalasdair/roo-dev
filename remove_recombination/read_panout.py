b38aff7d02c87f4584b856637172e007cdded758
Neil MacAlasdair <macalasdair@ubu-n-50797e58862a584d.ebi.ac.uk>
2025-11-17 15:50:02 +0000

    bugfix

--- remove_recombination/read_panout.py
+++ remove_recombination/read_panout.py
@@ -69,9 +69,13 @@ def get_all_pairwise_diffs(pairs, filt_genes, alignment_directory, threads, gpu)
         iso1_alnrows = np.array([alignment_isolate_row_lookup_dics[gene][iso1] for gene in shared_genes])
         iso2_alnrows = np.array([alignment_isolate_row_lookup_dics[gene][iso2] for gene in shared_genes])
         
-        dist_matrices = np.array([allval_pairwise[gene][0] for gene in shared_genes])
-        length_matrices = np.array([allval_pairwise[gene][1] for gene in shared_genes])
+        dist_matrices = [allval_pairwise[gene][0] for gene in shared_genes]
+        length_matrices = [allval_pairwise[gene][1] for gene in shared_genes]
         
+        dist_matrices = np.array(dist_matrices)
+        length_matrices = np.array(length_matrices)
+        print(dist_matrices.shape)
+        print(length_matrices.shape)
         
         isolate_gene_names = gene_names[shared_genes]
         gene_dists = dist_matrices[np.arange(len(shared_genes)), iso1_alnrows, iso2_alnrows]
