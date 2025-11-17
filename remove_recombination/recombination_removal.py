4999c8e79d3f71755541c3dd2584102ad9a01f8b
Neil MacAlasdair <macalasdair@ubu-n-50797e58862a584d.ebi.ac.uk>
2025-11-17 21:00:49 +0000

    remove profiling code

--- remove_recombination/recombination_removal.py
+++ remove_recombination/recombination_removal.py
@@ -1,7 +1,6 @@
 import os
 import math
 import decimal
-import cProfile
 
 from joblib import Parallel, delayed
 
@@ -68,8 +67,8 @@ def main():
         raise ValueError("Method must be one of [bayesian, frequentist]")
         
     #Load in relevant info from genes
-    pairs, pairwise_differences = cProfile.runctx("parse_pangenome(args.outdir, args.n_cpu, args.gpu)",
-                                                  globals(), locals())
+    pairs, pairwise_differences = parse_pangenome(args.outdir, args.n_cpu, args.gpu)
+    
     #Order genes from least snps/length to greatest snps/length
     ordered_pairs = order_pairwise_diffs(pairwise_differences)
     
