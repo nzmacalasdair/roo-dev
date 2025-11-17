3afb73056578880686d77b1719627b2d40bba9b4
Neil MacAlasdair <macalasdair@ubu-n-50797e58862a584d.ebi.ac.uk>
2025-11-17 18:52:34 +0000

    bugfix

--- remove_recombination/recombination_removal.py
+++ remove_recombination/recombination_removal.py
@@ -68,7 +68,7 @@ def main():
         raise ValueError("Method must be one of [bayesian, frequentist]")
         
     #Load in relevant info from genes
-    pairs, pairwise_differences = cProfile.run(parse_pangenome(args.outdir, args.n_cpu, args.gpu))
+    pairs, pairwise_differences = cProfile.run("parse_pangenome(args.outdir, args.n_cpu, args.gpu)")
     #Order genes from least snps/length to greatest snps/length
     ordered_pairs = order_pairwise_diffs(pairwise_differences)
     
